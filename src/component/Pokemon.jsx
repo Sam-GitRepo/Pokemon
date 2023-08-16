e790945aa38ac004667dddc093c121fe31d73bfe
Sam <smarttsamy@gmail.com>
2023-08-16 14:05:09 +0530

    fetch

--- src/component/Pokemon.jsx
+++ src/component/Pokemon.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Pokemon = () => {
+    const [pokemon, setPokemon] = useState([]);
+
+    const fetchPokemon = async ()   => {
+        try{
+            const response = await fetch('https://content.newtonschool.co/v1/pr/64ccef982071a9ad01d36ff6/pokemonspages1')
+            const data = await response.json()
+            // console.log(data)
+            setPokemon(data);
+            console.log("pokemon",pokemon);
+        }
+        catch{
+            console.log("error");
+        }
+    }
+    const loadPokemon =  () => {
+        
+    }
+    fetchPokemon()
   return (
-    <div>Pokemon</div>
+   <>
+   
+   </>
   )
 }
 
